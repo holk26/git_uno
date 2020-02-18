e283fa24d74084d49c0bd4a2f121970d2d2af248
holk <homero9726@gmail.com>
2020-02-17 22:09:00 -0500

    proyecto en githud

---
+++ app/src/main/java/com/holk/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.holk.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+        hola()
+    }
+
+    private fun hola() {
+
+    }
+}
